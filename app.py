4679745084fbbbb799b160f2395d33ce6beeca60
St-Lotus <stlotusittgi@gmail.com>
2026-03-17 16:44:17 +0630

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ def handle_file(data):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True, port=10000)
+    socketio.run(app, host='0.0.0.0', port=10000)
